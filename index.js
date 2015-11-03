15959c98a07e450a25d64489a088dac7a9eecd33
walid <walid.sa3d@gmail.com>
2015-11-03 12:19:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+
+const ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const digits = '0123456789'
+
+const hexdigits = '0123456789abcdefABCDEF'
+
+const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const lowercase = 'abcdefghijklmnopqrstuvwxyz'
+
+const octdigits = '01234567'
+
+const printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
+
+const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const whitespace = '\t\n\x0b\x0c\r'
+
+module.exports.ascii_letters = ascii_letters
+
+module.exports.ascii_lowercase = ascii_lowercase
+
+module.exports.ascii_uppercase = ascii_uppercase
+
+module.exports.digits = digits
+
+module.exports.hexdigits = hexdigits
+
+module.exports.letters = letters
+
+module.exports.lowercase = lowercase
+
+module.exports.octdigits = octdigits
+
+module.exports.printable = printable
+
+module.exports.punctuation = punctuation
+
+module.exports.uppercase = uppercase
+
+module.exports.whitespace = whitespace
